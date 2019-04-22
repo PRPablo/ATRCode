fad837093c8d007627aa68944db15537c8f66e40
pablo.prieto <pablo.prieto@artdecode.com.ar>
2019-04-22 17:54:20 -0300

    Revert "atr"
    This reverts commit 4b533be4c24c9ebb66e1fa8d8f52be8a415939fe.

--- src/atr/PablitoATR.java
+++ src/atr/PablitoATR.java
@@ -2,15 +2,4 @@ package atr;
 
 public class PablitoATR {
 
-	private Integer atr;
-
-	public Integer getAtr() {
-		return atr;
-	}
-
-	public void setAtr(Integer atr) {
-		this.atr = atr;
-	}
-	
-	
 }
