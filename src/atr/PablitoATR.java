4b533be4c24c9ebb66e1fa8d8f52be8a415939fe
pablo.prieto <pablo.prieto@artdecode.com.ar>
2019-04-22 17:19:39 -0300

    atr

--- src/atr/PablitoATR.java
+++ src/atr/PablitoATR.java
@@ -2,4 +2,15 @@ package atr;
 
 public class PablitoATR {
 
+	private Integer atr;
+
+	public Integer getAtr() {
+		return atr;
+	}
+
+	public void setAtr(Integer atr) {
+		this.atr = atr;
+	}
+	
+	
 }
